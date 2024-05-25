4d053a82f874df894731bdd20d86d304a4e330ec
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-25 11:51:22 +0200

    add:style card

--- server.php
+++ server.php
@@ -1,6 +1,12 @@
 <?php 
 $lista_dischi = file_get_contents("dischi.json");
 $dischi = json_decode($lista_dischi, true);
+for ($i=0; $i <count($dischi) ; $i++) { 
+    if(!isset($dischi[$i]["like"])){
+        $dischi[$i]["like"]= false;
+    };
+};
+file_put_contents("dischi.json", json_encode($dischi));
 $risposta = [
     "risultati" => $dischi,
     "success" => true,
