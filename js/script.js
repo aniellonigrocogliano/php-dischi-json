1069a93f7a5efd742a257228ad0d3d76b44c1d88
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-25 14:53:54 +0200

    add: funzione aggiungi nuovo album

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+        newAlbum:{},
         listaAlbum:[],
+        like:"",
+        newTitolo:"",
+        newAutore:"",
+        newAnno:"",
+        newGenere:"",
+        newImmagine:"",
     };
   },
   created() {
@@ -11,11 +18,36 @@ createApp({
       .get("http://localhost/boolean/php-dischi-json/server.php")
       .then((resp) => {
         this.listaAlbum = resp.data.risultati;
-        console.log(resp)
+        
       });
       
   },
   methods: {
-
+    addAlbum (){
+      const data = {
+        newAlbum: {
+        title: this.newTitolo,
+        author: this.newAutore,
+        genre: this.newGenere,
+        year: this.newAnno,
+        poster: this.newImmagine,
+      },
+      };
+      axios
+      .post("http://localhost/boolean/php-dischi-json/server.php", data, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      })
+      .then((resp) => {
+        this.listaAlbum = resp.data.risultati;
+      });
+      this.newTitolo="";
+      this.newAutore="";
+      this.newAnno="";
+      this.newGenere="";
+      this.newImmagine="";
+      console.log(this.listaAlbum)
+    },
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -1,6 +1,17 @@
 <?php 
 $lista_dischi = file_get_contents("dischi.json");
 $dischi = json_decode($lista_dischi, true);
+
+if (isset($_POST["newAlbum"])) {
+    $newAlbum = $_POST["newAlbum"];
+    $dischi[] = $newAlbum;
+    file_put_contents("dischi.json", json_encode($dischi));
+$risposta = [
+    "risultati" => $dischi,
+    "success" => true,
+  ];
+}
+
 for ($i=0; $i <count($dischi) ; $i++) { 
     if(!isset($dischi[$i]["like"])){
         $dischi[$i]["like"]= false;
